5cdb5206d31c184b0db96931ca18e80192aa05b2
giannisfourfouris <giannisfourfouris@gmail.com>
2018-10-11 19:51:49 +0300

    Fixied grill activity

--- CodeFolder/app/src/main/java/com/example/pcbill/microwaveapp/Grill_Activity.java
+++ CodeFolder/app/src/main/java/com/example/pcbill/microwaveapp/Grill_Activity.java
@@ -1,13 +1,30 @@
 package com.example.pcbill.microwaveapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Grill_Activity extends AppCompatActivity {
+
+    Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grill_);
+
+        buttonBack=(Button) findViewById(R.id.BackButton);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+
     }
 }
